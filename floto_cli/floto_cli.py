5ca137b2f0ccb706d8ebdc7486e6de14c1dfd1bb
Jacob Goldverg <jgoldverg@gmail.com>
2023-06-21 17:13:24 -0500

    removed extra imports

--- floto_cli/floto_cli.py
+++ floto_cli/floto_cli.py
@@ -1,7 +1,5 @@
 import click
-import os
 from device.device_cmds import device_cli
-from authentication import auth_cmd
 from collection.collection_cmd import collection_cli
 from authentication.auth_cmd import auth_cli
 cli = click.CommandCollection(sources=[device_cli, auth_cli, collection_cli])
